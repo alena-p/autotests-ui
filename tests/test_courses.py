4ff30fe16a722e7478d659b0b1704c37c19eeb0d
alena-p <sunny208@yandex.ru>
2025-10-19 14:42:33 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,22 +1,22 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 import pytest
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text("Courses")
+    courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
 
-        empty_courses_list_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-        expect(empty_courses_list_title).to_be_visible()
-        expect(empty_courses_list_title).to_have_text("There is no results")
+    empty_courses_list_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    expect(empty_courses_list_title).to_be_visible()
+    expect(empty_courses_list_title).to_have_text("There is no results")
 
-        empty_courses_list_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-        expect(empty_courses_list_icon).to_be_visible()
+    empty_courses_list_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+    expect(empty_courses_list_icon).to_be_visible()
 
-        empty_courses_list_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-        expect(empty_courses_list_text).to_be_visible()
-        expect(empty_courses_list_text).to_have_text("Results from the load test pipeline will be displayed here")
+    empty_courses_list_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
+    expect(empty_courses_list_text).to_be_visible()
+    expect(empty_courses_list_text).to_have_text("Results from the load test pipeline will be displayed here")
