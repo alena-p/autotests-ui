f7dadb2439711e6b32fc74eee1c1b0ccd8948275
alena-p <sunny208@yandex.ru>
2025-11-04 20:15:22 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -2,6 +2,9 @@ from playwright.sync_api import Page, expect
 
 from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
+from elements.text import Text
+from elements.button import Button
+from elements.link import Link
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
@@ -9,12 +12,15 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
 
-        self.page_title = page.get_by_test_id("authentication-ui-course-title-text")
-        self.registration_button = page.get_by_test_id("registration-page-registration-button")
-        self.login_link = page.get_by_test_id("registration-page-login-link")
+        self.page_title = Text(page, "authentication-ui-course-title-text", "Authentication page title")
+        self.registration_button = Button(
+            page, "registration-page-registration-button", "Registration button"
+        )
+        self.login_link = Link(page, "registration-page-login-link", "Login link")
+
 
     def click_registration_button(self):
         self.registration_button.click()
 
     def check_enabled_registration_button(self):
-        expect(self.registration_button).to_be_enabled()
+        self.registration_button.check_enabled()
