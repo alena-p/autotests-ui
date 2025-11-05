9a834e85308562b5d52a072138db98e7a715d333
alena-p <sunny208@yandex.ru>
2025-11-05 22:52:16 +0300

    Практическое применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- elements/file_input.py
+++ elements/file_input.py
@@ -3,6 +3,6 @@ from elements.base_element import BaseElement
 
 class FileInput(BaseElement):
 
-    def set_input_file(self, file, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def set_input_file(self, file, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         locator.set_input_files(file)
